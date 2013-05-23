4752535ddeecca8412cf77a27e5702366d061ce6
hirre <hirad.asadi@gmail.com>
2013-05-23 12:34:04 +0200

    Updated.

---
+++ ReleaseWin/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/hirasa/FOI-LOCAL/dev/EclipseProjects/maptag/lib/sqlite3.o
+
+LIBS := -lboost_filesystem-mgw47-mt-1_53 -lboost_regex-mgw47-mt-1_53 -lboost_system-mgw47-mt-1_53 -lboost_program_options-mgw47-mt-1_53
+
---
+++ TestWin/test/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../test/MapTagTest.cpp 
+
+OBJS += \
+./test/MapTagTest.o 
+
+CPP_DEPS += \
+./test/MapTagTest.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+test/%.o: ../test/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DTEST -I"C:\lib\boost\boost_1_53_0" -I"C:\Users\hirasa\FOI-LOCAL\dev\EclipseProjects\maptag\include" -O3 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
