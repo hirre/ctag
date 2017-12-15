29d1c7b4305484447586ca8a65f7ba15cbadaa2b
hirada <hirad.asadi@rco.se>
2017-12-15 10:43:02 +0100

    Removed copyright notice.

--- src/include/Error.hpp
+++ src/include/Error.hpp
@@ -5,21 +5,6 @@
  *
  *  Created on: 20 maj 2013
  *  E-mail: hirad.asadi@gmail.com
- *
- *  Copyright (C) 2013  Hirad Asadi
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef ERROR_HPP_
